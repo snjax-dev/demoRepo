80d71434ae1717bf8e1733687fa20d60ce19749e
snjax-dev <jadhavshahaji12@gmail.com>
2020-07-17 01:05:47 +0530

    added new codes to new branch

--- src/demo1/Class1.java
+++ src/demo1/Class1.java
@@ -5,4 +5,8 @@ public class Class1 {
 	public void add() {
 		System.out.println("added");
 	}
+	
+	public void multi() {
+		System.out.println("multiplied");
+	}
 }
--- src/demo2/Class2.java
+++ src/demo2/Class2.java
@@ -5,4 +5,8 @@ public class Class2 {
 	public void sub() {
 		System.out.println("subtracted");
 	}
+	
+	public void div() {
+		System.out.println("divided");
+	}
 }
